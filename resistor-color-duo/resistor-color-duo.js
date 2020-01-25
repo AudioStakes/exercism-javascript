e979d3a68a26156ed30a81c0dd08608e5b55570b
AudioStakes <daisuke.sato@enishi-tech.com>
2020-01-25 23:28:09 +0900

    2nd solution for resistor-color-duo

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,5 +1,3 @@
-export const decodedValue = (color_array) => {
-  return Number(COLORS.indexOf(color_array[0]).toString() + COLORS.indexOf(color_array[1]).toString());
-};
+const COLORS = ['black','brown','red','orange','yellow','green','blue','violet','grey','white'];
 
-export const COLORS = ['black','brown','red','orange','yellow','green','blue','violet','grey','white'];
+export const decodedValue = colors => colors.slice(0, 2).map(c => COLORS.indexOf(c)).join('') | 0;
